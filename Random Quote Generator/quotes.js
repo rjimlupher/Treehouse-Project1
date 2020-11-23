a93f31d833b9316fce32abe6075c78c07413c804
rjimlupher <64818086+rjimlupher@users.noreply.github.com>
2020-11-23 13:05:52 -0700

    Put existing file into folder

---
+++ Random Quote Generator/quotes.js
@@ -0,0 +1,39 @@
+// This is my first project for Treehouse. 
+
+const randomQuotes = [
+	{
+		quote: "He who makes a beast of himself gets rid of the pain of being a man.",
+		author: "Dr. Johnson"
+	},
+	{
+		quote: "Nothing is true; everything is permitted.",
+		author: "William S. Burroughs"
+	},
+	{
+		quote: "Whenever you find yourself on the side of the majority, it is time to pause and reflect.",
+		author: "Mark Twain"
+	},
+	{
+		quote: "Buy the ticket, take the ride.",
+		author: "Hunter S. Thompson"
+	},
+	{
+		quote: "May the days be aimless. Do not advance action according to a plan.",
+		author: "Don Delillo"
+	},
+	{
+		quote: "All human wisdom is summed up in two words: wait and hope.",
+		author: "Alexandre Dumas"
+	},
+	{
+		quote: "What greater gift than the love of a cat.",
+		author: "Charles Dickens"
+	},
+	{
+		quote: "It is never too late to be what you might have been.",
+		author: "George Eliot"
+	},
+
+]
+	
+function randomSelector 
